a6e686cba3792526fc99a59ce446d5b6fce1e767
ssg <ssg@sourtimes.org>
2016-05-15 16:45:16 -0700

    bump versions

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly:CLSCompliant(true)]
-// General Information about an assembly is controlled through the following 
+[assembly: CLSCompliant(true)]
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleBase32")]
-[assembly: AssemblyDescription("Base32 Encoding/Decoding Library")]
+[assembly: AssemblyTitle("SimpleBase")]
+[assembly: AssemblyDescription("Encoding/Decoding Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleBase32")]
-[assembly: AssemblyCopyright("Copyright ©  2014 Sedat Kapanoglu")]
+[assembly: AssemblyProduct("SimpleBase")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 Sedat Kapanoglu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -27,12 +26,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- test/Properties/AssemblyInfo.cs
+++ test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using NUnit.Framework;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Base32Test")]
+[assembly: AssemblyTitle("SimpleBaseTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Base32Test")]
+[assembly: AssemblyProduct("SimpleBaseTest")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
