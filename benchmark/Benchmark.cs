7ac545358fd59cf73bdc5f4256b7c58ce8f1987b
ssg <ssg@sourtimes.org>
2016-05-18 16:02:43 -0700

    add pretty printing to benchmark code

---
+++ benchmark/Benchmark.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace benchmark
+{
+    class Benchmark
+    {
+        public const int Iterations = 1000000;
+        
+        public const int BufSize = 64; // typical buffer size -- needs to be legit Base64 length w/o padding
+
+        public string Name { get; private set; }
+        public float Growth { get; private set; }
+        public TimeSpan EncodeTime;
+        public TimeSpan DecodeTime;
+        public Action<byte[]> EncodeFunc { get; private set; }
+        public Action<string> DecodeFunc { get; private set; }
+
+        public Benchmark(string name, float growth, Action<byte[]> encodeFunc, Action<string> decodeFunc)
+        {
+            this.Name = name;
+            this.Growth = growth;
+            this.EncodeFunc = encodeFunc;
+            this.DecodeFunc = decodeFunc;
+        }
+
+        private static string compare(TimeSpan bench, TimeSpan baseline)
+        {
+            if (bench > baseline)
+            {
+                return String.Format("{0:0.#}x slower", bench.TotalMilliseconds / baseline.TotalMilliseconds);
+            }
+            else
+            {
+                return String.Format("YAY! {0:0.#}x faster", baseline.TotalMilliseconds / bench.TotalMilliseconds);
+            }
+        }
+
+        public string GetEncodeText(Benchmark baseline)
+        {
+            return getPrintable(this.EncodeTime, baseline.EncodeTime);
+        }
+
+        public string GetDecodeText(Benchmark baseline)
+        {
+            return getPrintable(this.DecodeTime, baseline.DecodeTime);
+        }
+
+        private string getPrintable(TimeSpan time, TimeSpan baseline)
+        {
+            string result = String.Format("{0:s\\.ff}s", time);
+            if (time == baseline)
+            {
+                return result;
+            }
+            return result + " (" + compare(time, baseline) + ")";
+        }
+
+        public void TestEncode()
+        {
+            byte[] buf = new byte[BufSize];
+            buf[0] = 1;
+            buf[BufSize - 1] = 1; // avoid all-zero optimizations of Base58
+            Stopwatch w = Stopwatch.StartNew();
+            for (int n = 0; n < Iterations; n++)
+            {
+                EncodeFunc(buf);
+            }
+            w.Stop();
+            this.EncodeTime = w.Elapsed;
+        }
+
+        public void TestDecode()
+        {
+            string str = new String('a', BufSize);
+            Stopwatch w = Stopwatch.StartNew();
+            for (int n = 0; n < Iterations; n++)
+            {
+                DecodeFunc(str);
+            }
+            w.Stop();
+            this.DecodeTime = w.Elapsed;
+        }
+    }
+}
--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
-using System.Diagnostics;
 using SimpleBase;
 
 namespace benchmark
 {
     class Program
     {
+        static Benchmark[] benchmarks = new[]
+        {
+            new Benchmark(".NET Framework Base64", 1.33f,
+                (buf) => Convert.ToBase64String(buf),
+                (s) => Convert.FromBase64String(s)),
+
+            new Benchmark("SimpleBase Base16", 2f,
+                (buf) => Base16.EncodeUpper(buf),
+                (s) => Base16.Decode(s)),
+
+            new Benchmark("SimpleBase Base32 Crockford", 1.6f,
+                (buf) => Base32.Crockford.Encode(buf, padding: true),
+                (s) => Base32.Crockford.Decode(s)),
+
+            new Benchmark("SimpleBase Base58", 1.38f,
+                (buf) => Base58.Bitcoin.Encode(buf),
+                (s) => Base58.Bitcoin.Decode(s)),
+        };
+
         static void Main(string[] args)
         {
 #if DEBUG
@@ -13,62 +31,44 @@ namespace benchmark
 #else
             Console.WriteLine("Release mode");
 #endif
-            Encode();
-            Decode();
+            runTests();
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to exit");
             Console.ReadLine();
         }
 
-        const int iterations = 1000000;
-        const int testBufSize = 64; // typical buffer size -- needs to be legit Base64 length w/o padding
-
-        static TimeSpan test(Action func)
+        static void runTests()
         {
-            Stopwatch w = Stopwatch.StartNew();
-            for (int n = 0; n < iterations; n++)
+            Console.WriteLine();
+            Console.WriteLine("{0:#,#} iterations on {1} byte buffer (encode) / {1} character string (decode)",
+                Benchmark.Iterations, Benchmark.BufSize);
+            Console.WriteLine(@"
+Implementation              | Growth | Encode                   | Decode
+----------------------------|--------|--------------------------|------------------");
+            var baseline = benchmarks[0];
+            foreach (var benchmark in benchmarks)
             {
-                func();
+                Console.Write("{0,-28}| {1,-7}|", benchmark.Name, 
+                    String.Format("{0:0.##}x", benchmark.Growth));
+                beginTest();
+                benchmark.TestEncode();
+                endTest();
+                Console.Write(" {0,-25}|", benchmark.GetEncodeText(baseline));
+                beginTest();                
+                benchmark.TestDecode();
+                endTest();
+                Console.WriteLine(" {0, -25}", benchmark.GetDecodeText(baseline));
             }
-            w.Stop();
-            return w.Elapsed;
         }
 
-        static void Encode()
+        private static void endTest()
         {
-            byte[] buf = new byte[testBufSize];
-            buf[10] = 1;
-            var base64 = test(() => Convert.ToBase64String(buf));
-            var base32 = test(() => Base32.Crockford.Encode(buf, padding: true));
-            var base58 = test(() => Base58.Bitcoin.Encode(buf));
-            Dump("Encode benchmark", base64, base32, base58);
+            Console.Write(new String('\x08', 11));
         }
 
-        static void Decode()
+        private static void beginTest()
         {
-            var input = new string('a', testBufSize);
-            var base64 = test(() => Convert.FromBase64String(input));
-            var base32 = test(() => Base32.Crockford.Decode(input));
-            var base58 = test(() => Base58.Bitcoin.Decode(input));
-            Dump("Decode benchmark", base64, base32, base58);
-        }
-
-        static void Dump(string name, TimeSpan base64, TimeSpan base32, TimeSpan base58)
-        {
-            Console.WriteLine("*** " + name);
-            Console.WriteLine(" Baseline (.NET base64): {0}", base64);
-            Console.WriteLine(" SimpleBase Base32     : {0} ({1})", base32, compare(base32, base64));
-            Console.WriteLine(" SimpleBase Base58     : {0} ({1})", base58, compare(base58, base64));
-        }
-
-        private static string compare(TimeSpan bench, TimeSpan baseline)
-        {
-            if (bench > baseline)
-            {
-                return String.Format("{0:0.#}x slower", bench.TotalMilliseconds / baseline.TotalMilliseconds);
-            }
-            else
-            {
-                return String.Format("YAY! {0:0.#}x faster", baseline.TotalMilliseconds / bench.TotalMilliseconds);
-            }
+            Console.Write(" testing...");
         }
     }
 }
