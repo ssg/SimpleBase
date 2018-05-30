ff5c7c8c46069c9e42babc7e79cb5c3f5541139d
Sedat Kapanoglu <ssg@sourtimes.org>
2018-05-30 00:39:32 -0700

    bump version to 1.4.0

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleBase")]
-[assembly: AssemblyCopyright("Copyright © 2014-2016 Sedat Kapanoglu")]
+[assembly: AssemblyCopyright("Copyright © 2014-2018 Sedat Kapanoglu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1")]
-[assembly: AssemblyFileVersion("1.3.1")]
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
 
 [assembly: InternalsVisibleTo("SimpleBaseTest")]
