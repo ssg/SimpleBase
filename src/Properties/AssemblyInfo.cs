8dd5f1717b640823fd222cdd4b7f39e3fba66629
Sedat Kapanoglu <ssg@sourtimes.org>
2018-06-27 04:28:58 -0700

    bump version to 1.6.0

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("20592f17-d497-4dc4-9483-52911171f130")]
 
-[assembly: AssemblyVersion("1.5.1")]
-[assembly: AssemblyFileVersion("1.5.1")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
 
 [assembly: InternalsVisibleTo("SimpleBaseTest, PublicKey=0024000004800000140100000602000000240000525341310008000001000100612a9127f475475ffe4a714331c292aeb9e670d2ae238e869142c4517eaa1d4fc4ef75682cd225276aebff330203bd5ef86e3efb2f4fbe13977f94e715a60f5263ff357a8f610ffee280a446ff67c3800cd7d4ca20e7040c107a6b6a83da50427966a822c20c06c57b88e5dde58f3a44623e49ad0bf089a526b83100dc1fc78f6a75ffaa34e764075726e58e1e5fb01ec0c4c9274e99b3136cbbbd25f1fd0302b62bff2fffc3667a4214f58d19d5d06a30b9de959a054823fbb2c0d011f29c928ce9eeb93a88189a3e5ed135f4f3fff3aec36282e6d1acdf8a8c076ab8860498c4b734a19fd214187b4d9823f6b9b52d913307351f9cd9c66d29c258247824c1")]
