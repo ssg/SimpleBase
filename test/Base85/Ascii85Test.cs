a4f1c38390c2c8aa3dcf24c4bb400b39f4e7a140
Sedat Kapanoglu <ssg@sourtimes.org>
2018-11-16 23:21:35 -0800

    add empty input and whitespace cases to Base85 tests

--- test/Base85/Ascii85Test.cs
+++ test/Base85/Ascii85Test.cs
@@ -8,12 +8,27 @@ namespace SimpleBaseTest.Base85Test
     {
         private static readonly object[][] testVectors = new object[][]
         {
+            new object[] { new byte[] { }, "" },
             new object[] { new byte[] { 0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B }, "L/669[9<6." },
             new object[] { new byte[] { 0x11, 0x22, 0x33 }, "&L'\"" },
             new object[] { new byte[] { 77, 97, 110, 32 }, "9jqo^" },
             new object[] { new byte[] { 0, 0, 0, 0 }, "z" },
             new object[] { new byte[] { 0x20, 0x20, 0x20, 0x20 }, "y" },
         };
+
+        [Test]
+        [TestCaseSource(nameof(testVectors))]
+        public void Decode_Whitespace_IsIgnored(byte[] expectedOutput, string input)
+        {
+            string actualInput = String.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                actualInput += "  " + input[i];
+            }
+            actualInput += " ";
+            var result = Base85.Ascii85.Decode(input);
+            CollectionAssert.AreEqual(expectedOutput, result.ToArray());
+        }
 
         [Test]
         [TestCaseSource(nameof(testVectors))]
