f6354ed5cc97612d5bd2a366f1711274c5882e6c
Sedat Kapanoglu <ssg@sourtimes.org>
2018-12-04 13:03:23 -0800

    consolidate nuspec and AssemblyInfo in csproj

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,2 @@
 @echo off
-dotnet restore
-dotnet build -c Release
-xcopy /i /y src\bin\Release\netstandard2.0\* src\bin\Release\net45
-nuget pack SimpleBase.nuspec
+dotnet pack src -c Release -o ..
