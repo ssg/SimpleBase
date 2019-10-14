8fb2a9f1dbb7670680cc8487b904a077049abca6
Sedat Kapanoglu <ssg@sourtimes.org>
2019-10-14 13:10:52 -0700

    add test for EncodingAlphabet

---
+++ test/EncodingAlphabetTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using SimpleBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleBaseTest
+{
+    [TestFixture]
+    class EncodingAlphabetTest
+    {
+        [Test]
+        public void ctor_NullAlphabet_Throws()
+        {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            Assert.Throws<ArgumentNullException>(() => new Base16Alphabet(alphabet: null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        }
+    }
+}
