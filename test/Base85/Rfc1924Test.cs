0af84fc822b61a17daca7ba61e9566463ac105a8
Sedat Kapanoglu <ssg@sourtimes.org>
2020-12-12 17:27:56 -0800

    add potential future candidates

--- test/Base85/Rfc1924Test.cs
+++ test/Base85/Rfc1924Test.cs
@@ -1,11 +1,6 @@
 ﻿using NUnit.Framework;
 using SimpleBase;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleBaseTest.Base85Test
 {
