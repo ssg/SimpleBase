1f9c3ffbc95e3bd75c8153799b422d55f4fb0302
Sedat Kapanoglu <sedat@kapanoglu.com>
2025-05-03 13:34:10 -0700

    update copyrights

--- test/Multibase/MultibaseTest.cs
+++ test/Multibase/MultibaseTest.cs
@@ -1,5 +1,5 @@
 ﻿/*
-     Copyright 2014-2016 Sedat Kapanoglu
+     Copyright 2014-2025 Sedat Kapanoglu
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
