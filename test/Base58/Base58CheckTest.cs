f320802197f6f994d531fcf2a62c9ccc355716a6
Sedat Kapanoglu <sedat@kapanoglu.com>
2025-07-27 15:03:50 -0700

    move btcd license to its own file

--- test/Base58/Base58CheckTest.cs
+++ test/Base58/Base58CheckTest.cs
@@ -8,23 +8,8 @@ namespace SimpleBaseTest.Base58Test;
 [TestFixture]
 public class Base58CheckTest
 {
-    // the Base58Check test cases taken from btcutil package at https://github.com/btcsuite/btcutil
-    // ISC License
-    
-    //Copyright(c) 2013-2017 The btcsuite developers
-    //Copyright(c) 2016-2017 The Lightning Network Developers
-    
-    //Permission to use, copy, modify, and distribute this software for any
-    //purpose with or without fee is hereby granted, provided that the above
-    //copyright notice and this permission notice appear in all copies.
-    
-    //THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    //WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-    //MERCHANTABILITY AND FITNESS.IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    //ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    //WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-    //ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    //OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+    // the Base58Check test cases taken from btcutil package at https://github.com/btcsuite/btcd/tree/master/btcutil
+    // see LICENSE.btcd.txt for details
     static readonly object[] testData =
     [
         new object[] { 20, "", "3MNQE1X"},
